b5e882accc74ad211e482f48d252b8fe1db4a48d
Samuel Mwangi <crystalflow.mwangi@gmail.com>
2025-05-15 14:58:37 +0300

    Add example chain route to showcase middlewares.

--- server.js
+++ server.js
@@ -16,6 +16,25 @@ app.get("/old-page{.html}", (req, res) => {
     res.redirect(301, "/new-page.html");
 });
 
+const one = (req, res, next) => {
+    console.log("One then ...");
+    next();
+};
+
+const two = (req, res, next) => {
+    console.log("Two then ...");
+    next();
+};
+
+const three = (req, res, next) => {
+    console.log("Three!");
+    next();
+};
+
+app.get("/chain{.html}", [one, two, three], (req, res) => {
+    res.send("Chaining done.");
+});
+
 app.get("/{*splat}", (req, res) => {
     res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
