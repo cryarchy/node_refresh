f529be775e827769a0405726da197408dcd5995b
Samuel Mwangi <crystalflow.mwangi@gmail.com>
2025-05-15 16:22:43 +0300

    Add custom 404 responses depending on the request's accept header.

--- server.js
+++ server.js
@@ -59,8 +59,15 @@ app.get("/chain{.html}", [one, two, three], (req, res) => {
     res.send("Chaining done.");
 });
 
-app.get("/{*splat}", (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all("/{*splat}", (req, res) => {
+    res.status(404);
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
+        res.json({ error: "404 Not Found" });
+    } else {
+        res.type("txt").send("404 Not Found");
+    }
 });
 
 app.use(errorHandler);
