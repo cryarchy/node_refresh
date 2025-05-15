f9557f08b8573e3dfdbbc1d24b2119ce96eb001b
Samuel Mwangi <crystalflow.mwangi@gmail.com>
2025-05-15 13:01:11 +0300

    Manually resolve file to serve depending on the value of req.url.

--- server.js
+++ server.js
@@ -17,6 +17,18 @@ const PORT = process.env.PORT || 3500;
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+
+    let filePath;
+
+    if (req.url === "/" || req.url === "index.html") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        filePath = path.join(__dirname, "views", "index.html");
+        fs.readFile(filePath, "utf-8", (err, data) => {
+            if (err) throw err;
+            res.end(data);
+        });
+    }
 });
 
 server.listen(PORT, () => {
