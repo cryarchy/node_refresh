cf6f21adbec6f552ba557cfe294cdb93119154d1
Samuel Mwangi <crystalflow.mwangi@gmail.com>
2025-05-15 15:50:36 +0300

    Added builtin middlewares: urlencoded, json, static

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.get(["/", "/index{.html}"], (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"));
 });
